c276158a9336cae37c5d4a06135ec2511a38fb22
cerebralweb <paulocleon@gmail.com>
2020-10-15 17:31:47 -0500

    Updates

---
+++ precache-manifest.2d9cf6a6c0d86176999e5ae2716ac060.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e1577cd228adef7aebb0250e981220",
+    "url": "/votingapp1/index.html"
+  },
+  {
+    "revision": "5b3ead128b38514a7118",
+    "url": "/votingapp1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91edbd2ba76aca15d478",
+    "url": "/votingapp1/static/js/2.97dbaa3d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/votingapp1/static/js/2.97dbaa3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b3ead128b38514a7118",
+    "url": "/votingapp1/static/js/main.c014b2da.chunk.js"
+  },
+  {
+    "revision": "524d580b771a210df747",
+    "url": "/votingapp1/static/js/runtime-main.aa1023d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/votingapp1/static/media/logo.5d5d9eef.svg"
+  }
+]);
